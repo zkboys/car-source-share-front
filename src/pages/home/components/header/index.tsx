4510583d8998dac5eeaf98273e3c01473a5c3286
zkboys <876213774@qq.com>
2025-10-31 22:07:51 +0800

    :art: 整理代码

--- src/pages/home/components/header/index.tsx
+++ src/pages/home/components/header/index.tsx
@@ -2,7 +2,7 @@ import s from "./index.module.less";
 export function Header() {
   return (
     <div className={s.root}>
-      <img className={s.logo} src="https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=60" alt="logo" />
+      <img className={s.logo} src="/logo2x.png" alt="logo" />
       <div className={s.title}>卡泰驰汽车出口</div>
     </div>
   );
